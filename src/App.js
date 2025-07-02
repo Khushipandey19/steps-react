50e8a066d95b450d4f8c1e2ebfab96f651b86de8
Khushipandey19 <khushipandey1231@gmail.com>
2025-07-02 15:30:43 +0530

    using of useState for updating step

--- src/App.js
+++ src/App.js
@@ -6,19 +6,31 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
+
 export default function App() {
-  const step=1;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    // alert("Hello")
+    if (step > 1) setStep(() => step - 1)
+  }
+
+  function handleNext() {
+    // alert("noo")
+    if (step > 3) setStep(() => step + 1)
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step>=1?"active":""}`}>1</div>
-        <div className={`${step>=2?"active":""}`}>2</div>
-        <div className={`${step>=3?"active":""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-      <p className="message">Steps :{step}:{messages[step-1]}</p>
+      <p className="message">Steps :{step}:{messages[step - 1]}</p>
       <div className="buttons">
-        <button style={{backgroundColor:"#7950f2",color:"FFF"}}>Previous</button>
-        <button style={{backgroundColor:"#7950f2",color:"FFF"}}>Next</button>
+        <button style={{ backgroundColor: "#7950f2", color: "FFF" }} onClick={handlePrevious}>Previous</button>
+        <button style={{ backgroundColor: "#7950f2", color: "FFF" }} onClick={handleNext}>Next</button>
       </div>
     </div>
   );
